bc66e8fbd58b0eb533cb0c759a6a55542d8cb238
vozzhaevamv <mashazhukova2311@gmail.com>
2024-10-01 10:17:54 +1000

    task2.2.py

--- Homework2/task2.2.py
+++ Homework2/task2.2.py
@@ -1,3 +1,3 @@
 persons = [('John', 20), ('Nick', 15), ('Kate', 25),('Stif',19)]
-sorted_data_list = sorted((i for i in persons if i[1]>18), key=lambda x: x[1])
+sorted_data_list = sorted((i for i in persons if i[1]>18))
 print (sorted_data_list)
