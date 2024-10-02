bebca3b3e95eef02a6dbb2aab641129fafb1a994
vozzhaevamv <mashazhukova2311@gmail.com>
2024-10-02 10:40:33 +1000

    2.5.py

--- Homework2/task2.5.py
+++ Homework2/task2.5.py
@@ -1,6 +1,6 @@
 set1 = {1, 2, 3}
 set2 = {3, 4, 5}
 set3 = {2, 3, 4}
-intersection_set = set1.intersection(set2)
-intersection_set2 = intersection_set.intersection(set3)
+intersection_set = set1.intersection(set2) #Сравнение множеств на наличие пересечений в set1 и set2
+intersection_set2 = intersection_set.intersection(set3) #Сравнение множеств на наличие пересечений в (intersection_set) и set3
 print(intersection_set2)
