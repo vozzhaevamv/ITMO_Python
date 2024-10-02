b4f99dbcc1e7e09962a9be6aecf1d55a627aa7a1
vozzhaevamv <mashazhukova2311@gmail.com>
2024-10-02 10:36:31 +1000

    task2.2.py

--- Homework2/task2.2.py
+++ Homework2/task2.2.py
@@ -1,3 +1,3 @@
 persons = [('John', 20), ('Nick', 15), ('Kate', 25),('Stif',19)]
-sorted_data_list = sorted((i for i in persons if i[1]>18))
+sorted_data_list = sorted((i for i in persons if i[1]>18)) #создаем список с условием сортировки по элементу возраста в кортежах
 print (sorted_data_list)
