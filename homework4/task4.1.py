aa4db91e1609ed6547c7052a47c9cf543359b250
vozzhaevamv <mashazhukova2311@gmail.com>
2024-10-16 19:47:36 +1000

    task4.1.py

--- homework4/task4.1.py
+++ homework4/task4.1.py
@@ -1,20 +1,22 @@
-file_path = open(r"C:\Users\Masha\Desktop\text.txt", "r")
-with open(r"C:\Users\Masha\Desktop\text.txt", "r", encoding="utf-8") as file:
-    content = file_path.read()
-print("Файл считан:", content)
 import re
+file_way = r"C:\Users\Masha\Desktop\text.txt"
+file = open(file_way, "r", encoding="utf-8")
+content = file.read()
+file.close()
+print("Файл считан:", content)
 email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 emails = re.findall(email_pattern, content)
 print("Электронные почты:", emails)
 numbers = r'(\+?\d{1,3}[-.\s]?)?\(?\d{1,4}?\)?[-.\s]?\d{3}[-.\s]?\d{4}'
 numbers1 = re.findall(numbers, content)
 print ("Номера телефонов:", numbers1)
-output_file_path = open(r"C:\Users\Masha\Desktop\destination.txt", "r")
-with open(output_file_path, "w", encoding="utf-8") as output_file:
-    for email in emails:
-        output_file.write(email_pattern)
-    for number in numbers:
-        output_file.write(numbers)
-with open(output_file_path, "r", encoding="utf-8") as output_file:
-    content1 = output_file.read()
-print(content1)
+file.close()
+file_way2 = r"C:\Users\Masha\Desktop\textnew.txt"
+file2 = open(file_way2, "w", encoding="utf-8")
+file2.write("Электронные почты:")
+for email in emails:
+        file2.write(email_pattern)
+for number in numbers:
+        file2.write(numbers)
+
+print( file_way2)
