149ca6a1a99250c1bbf850f24614767804beaaf3
vozzhaevamv <mashazhukova2311@gmail.com>
2024-10-25 01:13:27 +1000

    Update task4.5.py

--- homework4/task4.5.py
+++ homework4/task4.5.py
@@ -5,5 +5,5 @@ characters = r'!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
 all_characters = letters+letters2+digits+characters
 length = int(input("Введите желаемое количество случайных символов: "))
 import random
-random_characters = ''.join(random.choices(all_characters, x=length))
+random_characters = ''.join(random.choices(all_characters, k=length))
 print(f"Случайные символы: {random_characters}")
